51b9ceeb8d8877f4d167963805510d551a0f8337
IgorSamoylov <igorletterbox@gmail.com>
2022-02-17 00:33:56 +0500

    1.2

--- C++ FILE STREAMS/C++ FILE STREAMS.cpp
+++ C++ FILE STREAMS/C++ FILE STREAMS.cpp
@@ -1,15 +1,15 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 using namespace std;
+
+const string FILE_PATH = "T:/TEST C++ OUTPUT.txt";
 
 int main()
 {
     ofstream output_file_stream; // Создаём объект выводного файлового потока 
 
     // Вызываем у объекта метод open и передаём туда адрес файла
-    output_file_stream.open("D:/TEST C++ OUTPUT.txt");
+    output_file_stream.open(FILE_PATH);
 
     // Записываем в стрим строку, используя оператор передачи в поток <<
     output_file_stream << "CAT VASYA!!!";
@@ -20,7 +20,7 @@ int main()
 
 
     ifstream input_file_stream;
-    input_file_stream.open("D:/TEST C++ OUTPUT.txt");
+    input_file_stream.open(FILE_PATH);
     char buffer[50];
     input_file_stream >> buffer; // Так можно считать одно слово
     cout << buffer << endl;
@@ -32,7 +32,7 @@ int main()
     input_file_stream.seekg(ios_base::beg);  
 
     // Затираем массив буфера функцией memset
-    memset(buffer, 0, 50);
+    memset(buffer, 0, 50 * sizeof(char));
 
     // read - то же самое, что и getline, но нельзя задать разделитель, используется для бинарников
     input_file_stream.read(buffer, 50); 
@@ -43,11 +43,11 @@ int main()
 
 
 
-
-    /* Использование старого способа работы с файлами - 
-     Функция fopen возвращает указатель на структуру FILE */
+    /*
+     Использование старого способа работы с файлами - 
+     Функция fopen возвращает указатель на структуру FILE 
     FILE** file{};
-    fopen_s(file, "D:/TEST C++ OUTPUT.txt", "rw" );
+    fopen_s(file, FILE_PATH.c_str(), "rw" );
 
     putc('F', *file); // Запись одного символа
     _putw(777, *file); // Запись одного числа
@@ -57,6 +57,6 @@ int main()
     fwrite(cat_ptr, sizeof('C'), 20, *file); // Запись блока памяти, представленного указателем
 
     fclose(*file);
-   
+   */
 }
 
